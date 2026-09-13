5127bf3a01828513e8ac0c7d4d594d51aa469b48
Rezanur2 <mdrezanurrahman2@gmail.com>
2026-09-13 12:22:09 +0600

    feat: build responsive Hero section and fixed navbar bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import Banner from "./components/Banner"
 import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <Banner />
     </>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,14 +9,14 @@ const Navbar = () => {
                     <img src={Logo} className='w-6 md:w-7 lg:w-9 xl:w-11' alt="Dev Stack Logo" />
                     <h2 className='text-[14px] sm:text-[16px] md:text-[18px] lg:text-[22px] xl:text-[24px] font-bold text-center ml-1 md:ml-1.5 tracking-tighter'>Dev <span className='text-[#DB2777]'>Stack</span></h2>
                 </div>
-                <ul className='hidden md:flex text-[12px] md:text-sm lg:text-base xl:text-lg md:gap-3 lg:gap-6 font-medium text-[#475569] '>
+                <ul className='hidden md:flex text-[12px] md:text-sm lg:text-base max-lg:pl-15 xl:text-lg md:gap-3 lg:gap-6 font-medium text-[#475569] '>
                     <li><a className='hover:text-[#DB2777] hover:bg-transparent transition-all duration-300' href="#">Home</a></li>
                     <li><a className='hover:text-[#DB2777] hover:bg-transparent transition-all duration-300' href="#">Technologies</a></li>
                     <li><a className='hover:text-[#DB2777] hover:bg-transparent transition-all duration-300' href="#">Projects</a></li>
                     <li><a className='hover:text-[#DB2777] hover:bg-transparent transition-all duration-300' href="#">About</a></li>
                     <li><a className='hover:text-[#DB2777] hover:bg-transparent transition-all duration-300' href="#">Contact</a></li>
                 </ul>
-                <div className='flex gap-0 sm:gap-1 lg:gap-3 text-xs md:text-sm lg:text-base xl:text-lg'>
+                <div className='flex gap-0 sm:gap-1 lg:gap-2 text-xs md:text-sm lg:text-base xl:text-lg'>
                     <button className=' text-[#334155] font-normal lg:font-medium border border-transparent hover:border-[#DB2777] px-3 md:px-5 md:py-2.5 rounded-[30px] lg:rounded-3xl hover:text-[#DB2777] hover:bg-transparent transition-colors duration-200'>Sign In</button>
                     <button className='text-white border bg-[#D91B7E] hover:text-[#DB2777] hover:bg-transparent transition-colors duration-300 font-normal lg:font-medium px-3 md:px-5 py-1 md:py-2.5 rounded-[30px]  lg:rounded-3xl' >Sign Up</button>
                 </div>
